338f18c7595d1b429282a45beccf91f0e7991266
An Tran <thuyan1149@gmail.com>
2020-12-29 16:06:23 +0200

    set up

---
+++ graphql/resolvers.js
@@ -0,0 +1,22 @@
+const { User } = require("../models");
+
+module.exports = {
+	Query: {
+		// getUsers: async () => {
+		//   try {
+		//     const users = await User.findAll()
+
+		//     return users
+		//   } catch (err) {
+		//     console.log(err)
+		//   }
+		// },
+		getUsers: () => {
+			const users = [
+				{ username: "an", email: "an@an" },
+				{ username: "an", email: "an@an" },
+			];
+			return users;
+		},
+	},
+};
