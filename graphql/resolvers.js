5b984e9dbd90f6d553b223340e4ef65426a0f94f
An Tran <thuyan1149@gmail.com>
2020-12-30 02:11:21 +0200

    added login query

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,10 @@
 const bcrypt = require("bcryptjs");
-const { UserInputError } = require("apollo-server");
+const { UserInputError, AuthenticationError } = require("apollo-server");
+const jwt = require("jsonwebtoken");
+const { Op } = require("sequelize");
 
 const { User } = require("../models");
+const { JWT_SECRET } = require("../config/env.json");
 
 module.exports = {
 	Query: {
@@ -12,6 +15,50 @@ module.exports = {
 				return users;
 			} catch (err) {
 				console.log(err);
+			}
+		},
+		login: async (_, args) => {
+			//#4 o doan 16:30
+			const { username, password } = args;
+			let errors = {};
+
+			try {
+				if (username.trim() === "")
+					errors.username = "username must not be empty";
+				if (password === "") errors.password = "password must not be empty";
+
+				if (Object.keys(errors).length > 0) {
+					throw new UserInputError("bad input", { errors });
+				}
+
+				const user = await User.findOne({
+					where: { username },
+				});
+
+				if (!user) {
+					errors.username = "user not found";
+					throw new UserInputError("user not found", { errors });
+				}
+
+				const correctPassword = await bcrypt.compare(password, user.password);
+
+				if (!correctPassword) {
+					errors.password = "password is incorrect";
+					throw new AuthenticationError("password is incorrect", { errors });
+				}
+
+				const token = jwt.sign({ username }, JWT_SECRET, {
+					expiresIn: 60 * 60,
+				});
+
+				return {
+					...user.toJSON(),
+					createdAt: user.createdAt.toISOString(),
+					token,
+				};
+			} catch (err) {
+				console.log(err);
+				throw err;
 			}
 		},
 	},
