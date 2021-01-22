780c5f609da7abb88c1db1c22618add1ee9a94fc
An Tran <thuyan1149@gmail.com>
2021-01-22 23:49:06 +0200

    get data and dispatch action by using context

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,39 +1,10 @@
-import React, { useEffect, useState, Fragment } from "react";
-import { Row, Col, Button, Image } from "react-bootstrap";
+import React, { useState, Fragment } from "react";
+import { Row, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { gql, useQuery, useLazyQuery } from "@apollo/client";
 
 import { useAuthDispatch } from "../../context/auth";
 import Users from "./Users";
-
-const GET_USERS = gql`
-	query getUsers {
-		getUsers {
-			username
-			createdAt
-			imageUrl
-			latestMessage {
-				uuid
-				from
-				to
-				content
-				createdAt
-			}
-		}
-	}
-`;
-
-const GET_MESSAGES = gql`
-	query getMessages($from: String!) {
-		getMessages(from: $from) {
-			uuid
-			from
-			to
-			content
-			createdAt
-		}
-	}
-`;
+import ChatContent from "./ChatContent";
 
 export default function Home({ history }) {
 	const dispatch = useAuthDispatch();
@@ -43,51 +14,6 @@ export default function Home({ history }) {
 		dispatch({ type: "LOGOUT" });
 		history.push("/login");
 	};
-
-	const { loading, data, error } = useQuery(GET_USERS);
-
-	const [
-		getMessages,
-		{ loading: messagesLoading, data: messagesData },
-	] = useLazyQuery(GET_MESSAGES);
-
-	useEffect(() => {
-		if (selectedUser) {
-			getMessages({ variables: { from: selectedUser } });
-		}
-	}, [selectedUser]);
-
-	if (messagesData) console.log(messagesData.getMessages);
-
-	let usersMarkup;
-	if (!data || loading) {
-		usersMarkup = <p>Loading..</p>;
-	} else if (data.getUsers.length === 0) {
-		usersMarkup = <p>No users have joined yet</p>;
-	} else if (data.getUsers.length > 0) {
-		usersMarkup = data.getUsers.map((user) => (
-			<div
-				className="d-flex p-3"
-				key={user.username}
-				onClick={() => setSelectedUser(user.username)}
-			>
-				<Image
-					src={user.imageUrl}
-					roundedCircle
-					className="mr-2"
-					style={{ width: 50, height: 50, objectFit: "cover" }}
-				/>
-				<div>
-					<p className="text-success">{user.username}</p>
-					<p className="font-weight-light">
-						{user.latestMessage
-							? user.latestMessage.content
-							: "You are now connected!"}
-					</p>
-				</div>
-			</div>
-		));
-	}
 
 	return (
 		<Fragment>
@@ -102,18 +28,10 @@ export default function Home({ history }) {
 					Logout
 				</Button>
 			</Row>
+
 			<Row className="bg-white">
 				<Users setSelectedUser={setSelectedUser} />
-				{/* <ChatContent /> */}
-				<Col xs={8}>
-					{messagesData && messagesData.getMessages.length > 0 ? (
-						messagesData.getMessages.map((message) => (
-							<p key={message.uuid}>{message.content}</p>
-						))
-					) : (
-						<p>Messages</p>
-					)}
-				</Col>
+				<ChatContent selectedUser={selectedUser} />
 			</Row>
 		</Fragment>
 	);
